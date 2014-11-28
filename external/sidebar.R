12aed66d3b3e6c802493f66dc098467ebd361ff6
andremarais <agmarais@gmail.com>
2014-11-28 19:05:05 +0200

    Create sidebar.R

---
+++ external/sidebar.R
@@ -0,0 +1,20 @@
+#source("external/uiHead.R",local=T)
+
+column(4,
+       wellPanel(
+         selectInput("server",
+                     label = "Server",
+                     choices = list("EU",
+                                    "US"),
+                     selected = "EU"),
+         textInput("BTName",
+                   label = "Battle Tag Name"),
+         textInput("BTCode",
+                   label = "Battle Tag Code")
+         
+         
+         ),
+       wellPanel(
+         uiOutput("herolist")
+       )
+)
